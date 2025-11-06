a64564d6f7ca907bdd393f9e64a71fe19058f856
Stephen Brown <padova02@gmail.com>
2025-11-05 22:20:36 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Project2App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.quarterRadioBtn = new System.Windows.Forms.RadioButton();
+            this.coinGroupBox = new System.Windows.Forms.GroupBox();
+            this.nickelRadioBtn = new System.Windows.Forms.RadioButton();
+            this.dimeRadioBtn = new System.Windows.Forms.RadioButton();
+            this.vendingMachineCheckedListBox = new System.Windows.Forms.CheckedListBox();
+            this.insertBtn = new System.Windows.Forms.Button();
+            this.buyBtn = new System.Windows.Forms.Button();
+            this.amountInsertedLbl = new System.Windows.Forms.Label();
+            this.amountValueLbl = new System.Windows.Forms.Label();
+            this.returnChangeBtn = new System.Windows.Forms.Button();
+            this.coinGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // quarterRadioBtn
+            // 
+            this.quarterRadioBtn.AutoSize = true;
+            this.quarterRadioBtn.Location = new System.Drawing.Point(6, 19);
+            this.quarterRadioBtn.Name = "quarterRadioBtn";
+            this.quarterRadioBtn.Size = new System.Drawing.Size(96, 17);
+            this.quarterRadioBtn.TabIndex = 0;
+            this.quarterRadioBtn.TabStop = true;
+            this.quarterRadioBtn.Text = "Quarter ($0.25)";
+            this.quarterRadioBtn.UseVisualStyleBackColor = true;
+            // 
+            // coinGroupBox
+            // 
+            this.coinGroupBox.Controls.Add(this.nickelRadioBtn);
+            this.coinGroupBox.Controls.Add(this.dimeRadioBtn);
+            this.coinGroupBox.Controls.Add(this.quarterRadioBtn);
+            this.coinGroupBox.Location = new System.Drawing.Point(12, 174);
+            this.coinGroupBox.Name = "coinGroupBox";
+            this.coinGroupBox.Size = new System.Drawing.Size(200, 93);
+            this.coinGroupBox.TabIndex = 1;
+            this.coinGroupBox.TabStop = false;
+            this.coinGroupBox.Text = "Coin Type";
+            // 
+            // nickelRadioBtn
+            // 
+            this.nickelRadioBtn.AutoSize = true;
+            this.nickelRadioBtn.Location = new System.Drawing.Point(6, 65);
+            this.nickelRadioBtn.Name = "nickelRadioBtn";
+            this.nickelRadioBtn.Size = new System.Drawing.Size(91, 17);
+            this.nickelRadioBtn.TabIndex = 2;
+            this.nickelRadioBtn.TabStop = true;
+            this.nickelRadioBtn.Text = "Nickel ($0.05)";
+            this.nickelRadioBtn.UseVisualStyleBackColor = true;
+            // 
+            // dimeRadioBtn
+            // 
+            this.dimeRadioBtn.AutoSize = true;
+            this.dimeRadioBtn.Location = new System.Drawing.Point(6, 42);
+            this.dimeRadioBtn.Name = "dimeRadioBtn";
+            this.dimeRadioBtn.Size = new System.Drawing.Size(85, 17);
+            this.dimeRadioBtn.TabIndex = 2;
+            this.dimeRadioBtn.TabStop = true;
+            this.dimeRadioBtn.Text = "Dime ($0.10)";
+            this.dimeRadioBtn.UseVisualStyleBackColor = true;
+            // 
+            // vendingMachineCheckedListBox
+            // 
+            this.vendingMachineCheckedListBox.FormattingEnabled = true;
+            this.vendingMachineCheckedListBox.Location = new System.Drawing.Point(12, 12);
+            this.vendingMachineCheckedListBox.Name = "vendingMachineCheckedListBox";
+            this.vendingMachineCheckedListBox.Size = new System.Drawing.Size(280, 154);
+            this.vendingMachineCheckedListBox.TabIndex = 2;
+            // 
+            // insertBtn
+            // 
+            this.insertBtn.Location = new System.Drawing.Point(56, 273);
+            this.insertBtn.Name = "insertBtn";
+            this.insertBtn.Size = new System.Drawing.Size(75, 23);
+            this.insertBtn.TabIndex = 3;
+            this.insertBtn.Text = "Insert";
+            this.insertBtn.UseVisualStyleBackColor = true;
+            this.insertBtn.Click += new System.EventHandler(this.insertBtn_Click);
+            // 
+            // buyBtn
+            // 
+            this.buyBtn.Location = new System.Drawing.Point(137, 273);
+            this.buyBtn.Name = "buyBtn";
+            this.buyBtn.Size = new System.Drawing.Size(75, 23);
+            this.buyBtn.TabIndex = 4;
+            this.buyBtn.Text = "Complete";
+            this.buyBtn.UseVisualStyleBackColor = true;
+            this.buyBtn.Click += new System.EventHandler(this.buyBtn_Click);
+            // 
+            // amountInsertedLbl
+            // 
+            this.amountInsertedLbl.AutoSize = true;
+            this.amountInsertedLbl.Location = new System.Drawing.Point(219, 193);
+            this.amountInsertedLbl.Name = "amountInsertedLbl";
+            this.amountInsertedLbl.Size = new System.Drawing.Size(87, 13);
+            this.amountInsertedLbl.TabIndex = 5;
+            this.amountInsertedLbl.Text = "Amount Inserted:";
+            // 
+            // amountValueLbl
+            // 
+            this.amountValueLbl.AutoSize = true;
+            this.amountValueLbl.Location = new System.Drawing.Point(219, 216);
+            this.amountValueLbl.Name = "amountValueLbl";
+            this.amountValueLbl.Size = new System.Drawing.Size(37, 13);
+            this.amountValueLbl.TabIndex = 6;
+            this.amountValueLbl.Text = "$ 0.00";
+            // 
+            // returnChangeBtn
+            // 
+            this.returnChangeBtn.Location = new System.Drawing.Point(219, 243);
+            this.returnChangeBtn.Name = "returnChangeBtn";
+            this.returnChangeBtn.Size = new System.Drawing.Size(75, 23);
+            this.returnChangeBtn.TabIndex = 7;
+            this.returnChangeBtn.Text = "Change";
+            this.returnChangeBtn.UseVisualStyleBackColor = true;
+            this.returnChangeBtn.Click += new System.EventHandler(this.returnChangeBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 335);
+            this.Controls.Add(this.returnChangeBtn);
+            this.Controls.Add(this.amountValueLbl);
+            this.Controls.Add(this.amountInsertedLbl);
+            this.Controls.Add(this.buyBtn);
+            this.Controls.Add(this.insertBtn);
+            this.Controls.Add(this.vendingMachineCheckedListBox);
+            this.Controls.Add(this.coinGroupBox);
+            this.Name = "Form1";
+            this.Text = "Vending Machine";
+            this.coinGroupBox.ResumeLayout(false);
+            this.coinGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton quarterRadioBtn;
+        private System.Windows.Forms.GroupBox coinGroupBox;
+        private System.Windows.Forms.RadioButton nickelRadioBtn;
+        private System.Windows.Forms.RadioButton dimeRadioBtn;
+        private System.Windows.Forms.CheckedListBox vendingMachineCheckedListBox;
+        private System.Windows.Forms.Button insertBtn;
+        private System.Windows.Forms.Button buyBtn;
+        private System.Windows.Forms.Label amountInsertedLbl;
+        private System.Windows.Forms.Label amountValueLbl;
+        private System.Windows.Forms.Button returnChangeBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Project2App
+{
+    public partial class Form1 : Form
+    {
+        private VendingMachine vendingMachine;
+        public Form1()
+        {
+            //Initial standard values for vending machine items
+            List<Product> vendingMachineItems = new List<Product>()
+            {
+                new Product("Soda", 125),
+                new Product("Chips", 100),
+                new Product("Candy", 75)
+            };
+            vendingMachine = new VendingMachine(vendingMachineItems);
+            InitializeComponent();
+            foreach (Product p in vendingMachineItems)
+            {
+                //vendingMachineCheckedListBox.Items.Add(p.Name + " - $ " + ((p.Price/100.00).ToString("F2")));
+                vendingMachineCheckedListBox.Items.Add($"{p.Name} - $ {(p.Price / 100.0):0.00}");
+            }
+        }
+
+        private void insertBtn_Click(object sender, EventArgs e)
+        {
+            if(quarterRadioBtn.Checked)
+            {
+                vendingMachine.InsertCoin(new Quarter());
+            }
+            else if(dimeRadioBtn.Checked)
+            {
+                vendingMachine.InsertCoin(new Dime());
+            }
+            else if(nickelRadioBtn.Checked)
+            {
+                vendingMachine.InsertCoin(new Nickel());
+            }
+            else { 
+                MessageBox.Show("Please select a coin to insert.");
+                return;
+            }
+            //double newAmount = (vendingMachine.CurrentBalance / 100.00);
+            amountValueLbl.Text = "$ " + (vendingMachine.CurrentBalance / 100.00).ToString("F2");
+        }
+
+        private void returnChangeBtn_Click(object sender, EventArgs e)
+        {
+            if(vendingMachine.CurrentBalance > 0)
+            {
+                MessageBox.Show("Returning change: $" + vendingMachine.ReturnChange().ToString("F2"));
+                //vendingMachine.ReturnChange();
+                amountValueLbl.Text = "$ 0.00";
+            }
+            else
+            {
+                MessageBox.Show("No balance to return.");
+            }
+        }
+
+        private void buyBtn_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Processing purchase...");
+            int totalCost = 0;
+            List<string> receipt = new List<string>();
+            if (vendingMachineCheckedListBox.CheckedItems.Count == 0)
+            {
+                MessageBox.Show("Please select at least one item to purchase.");
+                return;
+            }
+            foreach(string item in vendingMachineCheckedListBox.CheckedItems)
+            {
+                //MessageBox.Show("Selected item: " + item);
+                string selectedItemName = item.Split('-')[0].Trim();
+                foreach (Product p in vendingMachine.Products)
+                {
+                    if (p.Name == selectedItemName)
+                    {
+                        totalCost += p.Price;
+                        receipt.Add(selectedItemName);
+                    }
+                }
+            }
+            
+            if(totalCost != 0)
+            {
+                if(vendingMachine.CurrentBalance >= totalCost)
+                {
+                    vendingMachine.CurrentBalance -= totalCost;
+                    amountValueLbl.Text = "$ " + (vendingMachine.CurrentBalance / 100.00).ToString("F2");
+                    MessageBox.Show("You have purchased:\n" + string.Join(",\n", receipt) +
+                        "\n\nTotal: $" + (totalCost / 100.0).ToString("0.00"));
+                    vendingMachineCheckedListBox.ClearSelected();
+                }
+                else
+                {
+                    MessageBox.Show("Insufficient balance. Please insert more coins.");
+                }
+            }
+        }
+    }// end class Form1
+
+    class Coin
+    {
+        private int _value;
+        private int _weight;
+
+        public Coin(int value, int weight)
+        {
+            _value = value;
+            _weight = weight;
+        }
+        public int Value { get { return _value; } set { _value = value; } }
+        public int Weight { get { return _weight; } set { _weight = value; } }
+    }
+
+    class Quarter : Coin
+    {
+        public Quarter() : base(25, 4)
+        {
+
+        }
+    }
+
+    class Dime : Coin
+    {
+        public Dime() : base(10, 2){}
+    }
+
+    class Nickel : Coin
+    {
+        public Nickel() : base(5, 5)
+        {
+        }
+    }
+
+    class Product
+    {
+        private string _name;
+        private int _price;
+
+        public Product(string name, int price)
+        {
+            _name = name;
+            _price = price;
+        }
+        //Get and Set methods
+        public string Name { get { return _name; } set { _name = value; }}
+        public int Price { get { return _price; } set { _price = value; } }
+    }
+
+    class VendingMachine
+    {
+        const int MAX_CAPACITY = 50;
+        private List<Product> _products;
+        private int _currentBalance;
+        private int _currentCapacity;
+
+        public VendingMachine(List<Product> products)
+        {
+            _products = products;
+            _currentBalance = 0;
+        }
+        public void InsertCoin(Coin coin)
+        {
+            if(_currentCapacity + coin.Weight < MAX_CAPACITY)
+            {
+                _currentCapacity += coin.Weight;
+                _currentBalance += coin.Value;
+            }
+            else
+            {
+                MessageBox.Show("Vending Machine is full. Cannot accept more coins.");
+                return;
+            }
+        }
+        public int CurrentBalance{ get { return _currentBalance; } set { _currentBalance = value; } }
+        public int CurrentCapacity { get { return _currentCapacity; } set { _currentCapacity = value; } }
+
+        public List<Product> Products { get { return _products; } }
+
+        public double ReturnChange()
+        {
+            double change = _currentBalance / 100.00;
+            _currentBalance = 0;
+            return change;
+        }
+    }
+}// end namespace Project2App
